2f21e2a2e038e5ac3eaafe0dcc22598ec1766e3a
tukusu <ryomatukusu@gmail.com>
2015-10-30 19:33:23 +0900

    initial-commit

--- exec.js
+++ exec.js
@@ -62,6 +62,7 @@ var io=require('socket.io').listen(server);
    //fs.writeFileSync('hoge.json', source);
 });
 
-server.listen(8124);
+//server.listen(8124);
+server.listen(process.env.PORT || 5000)
  
 console.log('Server running at http://127.0.0.1:8124/');
